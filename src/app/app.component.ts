ede1b63fe8605b8462bfad41e604d9e5ecf35714
Wissam Hobeika <whobeika@gmail.com>
2017-01-04 16:47:36 -0500

    added one example for multiple dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: any[] = [
+    {
+      id: 1,
+      title: 'Item 1',
+      options: [
+        'Option 1',
+        'Option 2'
+      ]
+    },
+    {
+      id: 2,
+      title: 'Item 2',
+      options: [
+        'Option 1',
+        'Option 2',
+        'Option 3',
+        'Option 4'
+      ]
+    },
+    {
+      id: 3,
+      title: 'Item 3',
+      options: [
+        'Option 1',
+        'Option 2',
+        'Option 3'
+      ]
+    },
+    {
+      id: 4,
+      title: 'Item 4',
+      options: [
+        'Option 1',
+        'Option 2',
+        'Option 3',
+        'Option 4',
+        'Option 5',
+        'Option 6'
+      ]
+    }
+  ];
 }
